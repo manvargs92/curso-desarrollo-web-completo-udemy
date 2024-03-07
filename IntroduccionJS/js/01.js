5cbe215da98a62af7dae203ef34229cbaf387f5f
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-03-07 12:11:04 -0600

    01 - Variables con JavaScript.

---
+++ IntroduccionJS/js/01.js
@@ -0,0 +1,26 @@
+// Variables
+
+var producto = "Audífonos Gamer"; // declarar e inicializar una variable; var ya no se utiliza en código atual
+
+var disponible; // declarando la variable pero sin inicializarla
+
+producto = true; // reasignando el valor de la variable
+
+disponible = true;
+
+var producto1 = "Computadora", // declarar e inicializar múltiples variables
+    disponible1 = true,
+    categoria = "computadoras";
+
+// var 1disponible; // sintaxis no válida, las variables no pueden empezar con números
+// var -disponible; // sintaxis no válida
+var _disponible;
+var disponible_;
+
+// estilos para declarar variables
+var nombre_producto = "Monitor HD"; // snakecase
+var nombreProducto = "MonitorHD"; // camelCase
+var NombreProducto = "MonitorHD"; // PascalCase
+
+
+console.log(producto);
