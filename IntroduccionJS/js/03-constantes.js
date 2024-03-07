816c4ed38655c7a551bcd708b45fb5e59c22a73d
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-03-07 12:23:29 -0600

    03 - Constantes en JavaScript.

---
+++ IntroduccionJS/js/03-constantes.js
@@ -0,0 +1,27 @@
+// Variables
+
+const producto = "Audífonos Gamer"; // declarar e inicializar una constantes
+
+const disponible; // no se puede inicializar una constante sin valor
+
+producto = true; // no se puede reasignar el valor de una constante
+
+disponible = true; // no se puede reasignar el valor de una constante
+
+const producto1 = "Computadora", // declarar e inicializar múltiples constantes
+    disponible1 = true,
+    categoria = "computadoras";
+
+// const 1disponible; // sintaxis no válida, las constantes no pueden empezar con números
+// const -disponible; // sintaxis no válida
+const _disponible = true;
+const disponible_ = true;
+
+// estilos para declarar constantes
+const nombre_producto = "Monitor HD"; // snakecase
+const nombreProducto = "MonitorHD"; // camelCase
+const NombreProducto = "MonitorHD"; // PascalCase
+const NOMBRE_PRODUCTO = "Monitor HD";
+
+
+console.log(producto);
