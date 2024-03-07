c056f5ee0d6dcd56649e4871a117363bf5193ad1
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-03-07 12:16:53 -0600

    02 - Variables con let.

---
+++ IntroduccionJS/js/02-variables-con-let.js
@@ -0,0 +1,26 @@
+// Variables
+
+let producto = "Audífonos Gamer"; // declarar e inicializar una variable
+
+let disponible; // declarando la variable pero sin inicializarla
+
+producto = true; // reasignando el valor de la variable
+
+disponible = true;
+
+let producto1 = "Computadora", // declarar e inicializar múltiples variables
+    disponible1 = true,
+    categoria = "computadoras";
+
+// let 1disponible; // sintaxis no válida, las variables no pueden empezar con números
+// let -disponible; // sintaxis no válida
+let _disponible;
+let disponible_;
+
+// estilos para declarar variables
+let nombre_producto = "Monitor HD"; // snakecase
+let nombreProducto = "MonitorHD"; // camelCase
+let NombreProducto = "MonitorHD"; // PascalCase
+
+
+console.log(producto);
