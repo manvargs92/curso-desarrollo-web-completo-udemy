03e4337a1c20659bdcbb0f81567bdd1fe365d636
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-03-07 13:25:24 -0600

    06 - El objero Math en JS.

---
+++ IntroduccionJS/js/06-objeto-math.js
@@ -0,0 +1,17 @@
+// Objeto Math
+
+let resultado;
+
+resultado = Math.PI;
+resultado = Math.round(2.2); // .round() redondeo
+resultado = Math.ceil(2.1); // .ceil() redondea hacia arriba siempre
+resultado = Math.floor(2.9); // .floor() redondea hacia abajo siempre
+resultado = Math.sqrt(144); // .sqrt() raíz cuadrada
+resultado = Math.abs(-200); // .abs() convierte a número positivo
+resultado = Math.min(3, 5, 1, 8, 2, 10); // .min() encuentra el valor mínimo
+resultado = Math.max(3, 5, 1, 8, 2, 10); // .min() encuentra el valor máximo
+resultado = Math.random(); // .random() genera un número aleatorio de 0 a 1
+
+resultado = Math.floor(Math.random() * 5);
+
+console.log(resultado);
