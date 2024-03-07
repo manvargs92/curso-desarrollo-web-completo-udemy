56393505abdfdff48e505652b3f06f5c78049b3b
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-03-07 13:36:51 -0600

    07 - Controlar el orden de las operaciones.

---
+++ IntroduccionJS/js/07-orden-operaciones.js
@@ -0,0 +1,30 @@
+// Orden de las operaciones
+
+let resultado;
+
+resultado = 20 + 30 * 2;
+resultado = (20 + 30) * 2;
+
+resultado = (100 + 200 + 300) * .2; // un descuento
+
+resultado = (100 + 200 + 300) * 1.16; // un impuesto
+
+resultado = (100 + 200) * 1.05; // una comisión
+
+console.log(resultado);
+
+// Incrementos
+
+let puntaje = 10;
+
+puntaje++; // incrementos en 1
+console.log(puntaje);
+// console.log(++puntaje); // incremento en 1 en los console.log
+
+// Decrementos
+puntaje--;
+console.log(puntaje);
+
+// Incementos en n cantidad
+puntaje += 10;
+console.log(puntaje);
