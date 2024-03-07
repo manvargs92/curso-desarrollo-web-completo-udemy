a206fb03719d88b9c4a9fc12ea2f12ec62e9c779
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-03-07 13:09:36 -0600

    05 - Números y operadores.

---
+++ IntroduccionJS/js/05-numeros-y-operadores.js
@@ -0,0 +1,26 @@
+// Números
+
+const nombre = "Juan";
+const numero = "100";
+// en JS todos lo números son tratados como "número", no hay una forma de declarar el tipo de número
+const numero1 = 7;
+const numero2 = 6;
+const numero3 = 200.20;
+const numero4 = .1020;
+const numero5 = -5;
+
+console.log(nombre);
+console.log(numero1);
+console.log(numero2);
+
+// Operadores
+
+console.log(numero1 + numero2); // suma
+console.log(numero2 - numero1); // resta
+console.log(numero2 * numero1); // multiplicación
+console.log(numero2 / numero1); // división
+console.log(numero1 % numero2); // módulo (resíduo de la división)
+
+const cliente1 = "Paco";
+const cliente2 = "Luis";
+console.log(cliente1 * cliente2);
