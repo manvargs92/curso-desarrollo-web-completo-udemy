8e5a5ba92ebc1bc85eb5524f94e3c6cfbae30d21
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-03-07 12:52:30 -0600

    04 - Métodos para los strings.

--- IntroduccionJS/js/04-strings.js
+++ IntroduccionJS/js/04-strings.js
@@ -15,3 +15,20 @@ console.log(typeof(PRODUCTO3));
 
 const PRODUCTO4 = "Pantalla de 20\""; // escapar caracteres
 console.log(PRODUCTO4);
+
+const PRODUCTO5 = "Monitor HD";
+console.log(PRODUCTO5);
+
+// Length
+console.log(PRODUCTO5.length); // .length propiedad para saber la longitud del string
+
+const TWEET = "Aprendiendo JavaScript con el curso de Desarrollo Web Completo.";
+console.log(TWEET.length);
+
+// IndexOf
+console.log(TWEET.indexOf("JavaScript")); // .indexOf método para saber la posición de una palabra en un string, retorna un valor >= 0, o -1 en caso de que no la encuentre
+console.log(PRODUCTO5.indexOf("Tablet"));
+
+// Include
+console.log(TWEET.includes("JavaScript")); // .includes método para saber si hay una palabra en un string, retorna true o false
+console.log(PRODUCTO5.includes("Tablet"));
