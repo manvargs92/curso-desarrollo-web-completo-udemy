901dd312e44190483154dfa10692133cf14b4542
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-03-08 12:07:26 -0600

    08 - Concatenación y template strings.

---
+++ IntroduccionJS/js/08-concatenacion-y-template-strings.js
@@ -0,0 +1,9 @@
+// Concatenación y template strings
+
+const nombre = "Manuel";
+const email = "manuel@micorreo.com";
+
+console.log("Nombre cliente: " + nombre + ", Email: " + email); // concatenación
+
+// Template Strings / Strings Literals
+console.log(`Nombre cliente: ${nombre}, Email: ${email}`);
