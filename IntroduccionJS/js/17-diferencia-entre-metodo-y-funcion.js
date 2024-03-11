ff3ed173650053e74cadf4df040d33f96738145e
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-03-11 14:52:48 -0600

    17 - Diferencia entre un método y una función.

---
+++ IntroduccionJS/js/17-diferencia-entre-metodo-y-funcion.js
@@ -0,0 +1,8 @@
+// Diferencie entre un método y una función
+
+const numero1 = 20;
+const numero2 = "20";
+
+console.log(parseInt(numero2)); // parseInt() es una función
+
+console.log(número1.toString(numero1)); // .toString() es un método
