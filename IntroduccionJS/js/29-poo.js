fbd8386a930aa1a9f4d7db7ffaa5e8da0481114f
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-03-13 13:01:04 -0600

    29 - POO - Herencia.

--- IntroduccionJS/js/29-poo.js
+++ IntroduccionJS/js/29-poo.js
@@ -12,8 +12,8 @@ class Producto {
         return `El producto ${this.nombre} tiene un precio de $${this.precio}`;
     }
 
-    retornarPrecio() {
-        return this.precio;
+    obtenerPrecio() {
+        console.log(this.precio);
     }
 }
 
@@ -25,4 +25,31 @@ console.log(producto2);
 console.log(producto3);
 
 console.log(producto2.formatearProducto());
-console.log(producto2.retornarPrecio());
+console.log(producto2.obtenerPrecio());
+
+// Herencia
+// la herencia evita duplicar código
+
+class Libro extends Producto{ // hereda de la clase Producto el constructor y la fucnión formatearProducto
+    constructor(nombre, precio, isbn) {
+    //     this.nombre = nombre;
+    //     this.precio = precio;
+        super(nombre, precio);
+        this.isbn = isbn;
+    }
+
+    formatearProducto() {
+        return `${super.formatearProducto()} y su ISBN es ${this.isbn}`;
+    }
+
+    obtenerPrecio() {
+        super.obtenerPrecio();
+        console.log("Y sí hay en existencia.");
+    }
+}
+
+const libro = new Libro("JavaScript, la revolución", 120, "65465064043135103");
+
+console.log(libro);
+console.log(libro.formatearProducto());
+console.log(libro.obtenerPrecio());
