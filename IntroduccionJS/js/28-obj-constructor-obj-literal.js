06105f893453fe6e71b964c30fe6cf711ca7df18
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-03-13 11:51:39 -0600

    28 - prototype en JavaScript.

--- IntroduccionJS/js/28-obj-constructor-obj-literal.js
+++ IntroduccionJS/js/28-obj-constructor-obj-literal.js
@@ -14,11 +14,28 @@ function Producto(nombre, precio) { // clase Producto
     this.precio = precio;
 }
 
+// prototype - crear funciones que solo se utilizan en un objeto en específico
+
+Producto.prototype.formatearProducto = function() { // no se requiere que tome ningún parámetro porque ya está atado a Producto
+    return `El producto ${this.nombre} tiene un precio de $${this.precio}`
+}
+
+// function formatearProducto(producto) {
+//     return `El producto ${producto.nombre} tiene un precio de $${producto.precio}`
+// }
+
+
 const producto2 = new Producto("Monitor curvo de 49\"", 800); // instanciación de la clase Producto
 const producto3 = new Producto("Laptop", 800);
 
 console.log(producto2);
 console.log(producto3);
+
+// console.log(formatearProducto(producto2));
+console.log(producto2.formatearProducto());
+
+
+
 
 
 
@@ -29,8 +46,14 @@ function Alumno(nombre, edad, promedio, graduado) {
     this.graduado = graduado;
 }
 
+Alumno.prototype.imprimir = function () {
+    return "Hola!!";
+}
+
 const alumno1 = new Alumno("Paquito", "10", 8.3, false);
 const alumno2 = new Alumno("Juanita", "13", 7.8, true);
 
 console.log(alumno1);
 console.log(alumno2);
+
+console.log(alumno1.imprimir());
