b2afc061dad831aa41b7668de82ef7095a7d3e78
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-03-13 13:09:33 -0600

    30 - Try Catch.

---
+++ IntroduccionJS/js/30-try-catch.js
@@ -0,0 +1,16 @@
+// Try Catch
+
+const numero1 = 20;
+const numero3 = 30;
+
+console.log(numero1);
+
+// console.log(numero2); // la ejecución del código se cortaría en esta línea ya que daría un error
+
+try {
+    console.log(numero2);
+} catch (error) {
+    console.log(error);
+}
+
+console.log(numero3);
