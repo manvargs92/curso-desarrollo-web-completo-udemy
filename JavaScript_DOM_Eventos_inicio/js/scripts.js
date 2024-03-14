fba4db85410608bdbd453f548690820b58232c4f
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-03-14 11:17:29 -0600

    Seleccionar elementos con querySelector.

---
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -0,0 +1,15 @@
+// querySelector
+
+const heading = document.querySelector(".header__texto h2"); // querySelector retorna 0 o 1 elementos
+console.log(heading);
+
+heading.textContent = "Nuevo Heading"; // modificar contenido de la etiqueta
+
+heading.classList.add("nueva-clase"); // agregar una nueva clase a la etiqueta
+
+
+// querySelectorAll
+
+
+
+// getElementById
