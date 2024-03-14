fa94ec0b837d07652a36188f027a6a45f29ede89
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-03-14 11:35:20 -0600

    Seleccionar elementos con querySelectorAll.

--- JavaScript_DOM_Eventos_inicio/js/scripts.js
+++ JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -10,6 +10,15 @@ heading.classList.add("nueva-clase"); // agregar una nueva clase a la etiqueta
 
 // querySelectorAll
 
+const enlaces = document.querySelectorAll(".navegacion a"); // querySelectorAll retorna 0 o todos los elementos que concuerden con el selector
+console.log(enlaces);
+console.log(enlaces[0]); // seleccionar un elemento en específico
+
+enlaces[0].textContent = "Nuevo texto para Enlace";
+// document.querySelectorAll(".navegacion a")[0].textContent = "Nuevo texto para Enlace"; // hace lo mismo en una sola línea
+enlaces[0].href = "https://www.google.com/"; // modificar el hipervínculo de un enlace
+enlaces[0].classList.add("nueva-clase");
+enlaces[0].classList.remove("navegacion__enlace"); // remover una clase a la etiqueta
 
 
 // getElementById
