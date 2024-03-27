c5298102e27205eab4f0b69368c59c268be9db47
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-03-27 12:45:56 -0600

    Añadiendo botón para cerra imágen de la ventana modal.

--- FestivalMusica_inicio/build/js/app.js
+++ FestivalMusica_inicio/build/js/app.js
@@ -19,7 +19,7 @@ function crearGaleria() {
         `;
 
         // imagen.onclick = mostrarImagen(i);
-        imagen.onclick = function () { // función ejecutada como callback
+        imagen.onclick = function () { // función ejecutada como callback para ejecutar mas de una acción
             mostrarImagen(i);
         }
         
@@ -36,10 +36,29 @@ function mostrarImagen(id) {
         <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="Imagen galería">
     `;
 
+    /* Crea el overlay con la imágen */
     const overlay = document.createElement("DIV");
     overlay.appendChild(imagen);
     overlay.classList.add("overlay");
+    overlay.onclick = function () {
+        const body = document.querySelector("body");
+        body.classList.remove("fijar-body"); // remueve el bloqueo del scroll dando clic en cualquier parte
+        overlay.remove();
+    }
 
+    /* Botón para cerrar la ventana modal */
+    const cerrarModal = document.createElement("P");
+    cerrarModal.textContent = "X";
+    cerrarModal.classList.add("btn-cerrar");
+    cerrarModal.onclick = function () { // función ejecutada como callback para ejecutar mas de una acción
+        const body = document.querySelector("body");
+        body.classList.remove("fijar-body"); // remueve el bloqueo del scroll cuando se cierra la imágen con el botón X
+        overlay.remove();
+    }
+    overlay.appendChild(cerrarModal);
+
+    /* Añadir el ovelay al HTML */
     const body = document.querySelector("body");
     body.appendChild(overlay);
+    body.classList.add("fijar-body");
 }
