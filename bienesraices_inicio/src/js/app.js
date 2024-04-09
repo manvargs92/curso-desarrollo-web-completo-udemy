c2fde45043a6bf04b0f1e76c459d1d3ba0d4a3f6
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-04-09 12:02:36 -0600

    Creando una navegación responsive.

---
+++ bienesraices_inicio/src/js/app.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    eventListeners();
+});
+
+function eventListeners() {
+    const mobileMenu = document.querySelector(".mobile-menu");
+
+    mobileMenu.addEventListener("click", navegacionResponsive);
+}
+
+function navegacionResponsive() {
+    const navegacion = document.querySelector(".navegacion");
+
+    if (navegacion.classList.contains("mostrar")) {
+        navegacion.classList.remove("mostrar");
+    } else {
+        navegacion.classList.add("mostrar");
+    }
+    // navegacion.classList.toggle("mostrar");// hace lo mismo de arriba en una sola línea
+
+}
