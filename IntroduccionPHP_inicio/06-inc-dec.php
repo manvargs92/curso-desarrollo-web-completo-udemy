74a15e0e9e61b9be9562eeff42248837951defd7
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-04-10 09:25:26 -0600

    Incrementos y decrementos en PHP.

--- IntroduccionPHP_inicio/06-inc-dec.php
+++ IntroduccionPHP_inicio/06-inc-dec.php
@@ -1,6 +1,34 @@
 <?php include 'includes/header.php';
 
+/* Incremento en 1 */
+$numero1 = 30;
+$numero1++;
+echo $numero1;
 
+echo "<br>";
+
+echo ++$numero1;
+
+echo "<br>";
+
+/* Decremento en 1*/
+$numero2 = 30;
+$numero2--;
+echo $numero2;
+
+echo "<br>";
+
+echo --$numero2;
+
+echo "<br>";
+
+/* Incremento en n */
+echo $numero1 += 10;
+
+echo "<br>";
+
+/* Decremento en n */
+echo $numero2 -= 10;
 
 
 include 'includes/footer.php';
