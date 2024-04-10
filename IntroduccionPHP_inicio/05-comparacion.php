0e4982a0c5d9c7e307d1223de946205fb35d7c3e
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-04-10 09:16:44 -0600

    Comparadores en PHP.

--- IntroduccionPHP_inicio/05-comparacion.php
+++ IntroduccionPHP_inicio/05-comparacion.php
@@ -1,7 +1,36 @@
 <?php include 'includes/header.php';
 
+$numero1 = 20;
+$numero2 = 30;
+$numero3 = 30;
+$numero4 = "30";
 
+var_dump($numero1 > $numero2);
+echo "<br>";
 
+var_dump($numero1 < $numero2);
+echo "<br>";
+
+var_dump($numero1 >= $numero2);
+echo "<br>";
+
+var_dump($numero2 == $numero3);
+echo "<br>";
+
+var_dump($numero2 == $numero4);
+echo "<br>";
+
+var_dump($numero2 === $numero4);
+echo "<br>";
+
+var_dump($numero1 <=> $numero2); // el de la derecha es mayor, imprimirá -1
+echo "<br>";
+
+var_dump($numero2 <=> $numero3); // ambos son iguales, imprimirá 0
+echo "<br>";
+
+var_dump($numero2 <=> $numero1); // el de la izquieda es mayor, imprimirá 1
+echo "<br>";
 
 
 include 'includes/footer.php';
