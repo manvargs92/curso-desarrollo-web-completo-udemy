3b029d37806fa3155aec613420604734de861171
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-04-10 08:55:24 -0600

    Tipos de dato en PHP.

--- IntroduccionPHP_inicio/03-tipos-dato.php
+++ IntroduccionPHP_inicio/03-tipos-dato.php
@@ -1,6 +1,28 @@
 <?php include 'includes/header.php';
 
+/* Booleans */
+$logueado = true;
 
+var_dump($logueado);
 
+/* Enteros */
+$numeroEntero = 200;
+
+var_dump($numeroEntero);
+
+/* Flotantes */
+$flotante = 200.55;
+
+var_dump($flotante);
+
+/* Strings */
+$nombre = "Miguel";
+
+var_dump($nombre);
+
+/* Arreglos */
+$array = [];
+
+var_dump($array);
 
 include 'includes/footer.php';
