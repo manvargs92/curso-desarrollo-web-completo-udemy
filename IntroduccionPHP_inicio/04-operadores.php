72390993a64f51f61e78bf1a1c4312e5b3c6352c
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-04-10 09:06:45 -0600

    Números y operadores en PHP.

--- IntroduccionPHP_inicio/04-operadores.php
+++ IntroduccionPHP_inicio/04-operadores.php
@@ -1,6 +1,31 @@
 <?php include 'includes/header.php';
 
+$numero1 = 20;
+$numero2 = 10;
 
+/* Sumar */
+echo $numero1 + $numero2;
 
+echo "<br>";
+
+/* Restar */
+echo $numero1 - $numero2;
+
+echo "<br>";
+
+/* Multiplicar */
+echo $numero1 * $numero2;
+
+echo "<br>";
+
+/* Dividir */
+echo $numero1 / $numero2;
+
+echo "<br>";
+
+/* Potenciación */
+echo 2 ** 3;
+
+echo "<br>";
 
 include 'includes/footer.php';
