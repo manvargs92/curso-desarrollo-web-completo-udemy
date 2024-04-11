d733433620ccbabf62606a9ceac5e627dcd36eba
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-04-11 16:14:17 -0600

    Include y require en PHP.

--- IntroduccionPHP_inicio/17-require.php
+++ IntroduccionPHP_inicio/17-require.php
@@ -1,7 +1,12 @@
 <?php include 'includes/header.php';
 
+/* Formas de agregar un archivo externo */
 
+include "includes/header.php"; // usarlo cuando se quieran incluir otros templates
 
+require "includes/header.php"; // usarlo cuando hayan funciones críticas para la aplicación
+
+require_once "includes/header.php"; // revisa si el archivo ya fue incluido, y si ya lo fué, ignora esta línea de código
 
 
 
