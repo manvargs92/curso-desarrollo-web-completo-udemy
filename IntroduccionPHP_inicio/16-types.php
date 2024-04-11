dcfe1ff180af91a9b6c0349ded2cd6106200442b
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-04-11 15:47:43 -0600

    Funciones que retornan valores en PHP.

--- IntroduccionPHP_inicio/16-types.php
+++ IntroduccionPHP_inicio/16-types.php
@@ -1,5 +1,45 @@
-<?php include 'includes/header.php';
+<?php
+declare(strict_types=1);
+include 'includes/header.php';
 
+// función que retorna valores
+function usuarioAutenticado (bool $autenticado) : string { // : indica que tipo de dato será retornado
+    if ($autenticado) {
+        return "El usuario está autenticado";
+    } else {
+        return "No autenticado";
+    }
+}
+
+$usuario = usuarioAutenticado(false);
+
+echo $usuario;
+
+function imprimir () : void { //: void indica que no retornará ningún valor
+    echo "<br>Esta función solo imprime este string";
+}
+
+imprimir();
+
+function usuarioAutenticado2 (bool $autenticado) : ?string { // : ? indica que el tipo de dato retornado será opcional
+    if ($autenticado) {
+        return "El usuario está autenticado";
+    } else {
+        return null;
+    }
+}
+
+echo "<br>" . usuarioAutenticado2(false);
+
+function usuarioAutenticado3 (bool $autenticado) : string | int { // : | indica los tipos de datos que podrán ser retornados
+    if ($autenticado) {
+        return "El usuario está autenticado";
+    } else {
+        return 20;
+    }
+}
+
+echo "<br>" . usuarioAutenticado3(false);
 
 
 
