b9f5ba5881a26523a4a6b8b50f976a79fbb35419
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-04-11 16:31:13 -0600

    Codificar y decodificar JSON en PHP.

--- IntroduccionPHP_inicio/18.json.php
+++ IntroduccionPHP_inicio/18.json.php
@@ -1,7 +1,37 @@
 <?php include 'includes/header.php';
 
+$productos = [
+    [
+        'nombre' => 'Tablet',
+        'precio' => 200,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Televisión 24"',
+        'precio' => 400,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Monitor Curvo',
+        'precio' => 300,
+        'disponible' => false
+    ]
+];
 
+echo "<pre>";
+var_dump($productos);
 
+/* Convertir un arreglo a JSON */
+$json = json_encode($productos, JSON_UNESCAPED_UNICODE);
+
+/* Convertir un JSON a arreglo */
+$json_array = json_decode($json);
+
+var_dump($json);
+
+var_dump($json_array);
+
+echo "</pre>";
 
 
 
