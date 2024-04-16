dc7affdaf488ccb9156f346948be4576b5d62a6d
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-04-16 09:15:57 -0600

    HTML y el formulario para crear propiedades.

--- bienesraices_inicio/admin/propiedades/crear.php
+++ bienesraices_inicio/admin/propiedades/crear.php
@@ -7,6 +7,47 @@ incluirTemplate("header");
         <h1>Crear</h1>
 
         <a href="/admin" class="boton boton-verde">Volver</a>
+
+        <form class="formulario">
+            <fieldset>
+                <legend>Información General</legend>
+
+                <label for="titulo">Titulo:</label>
+                <input type="text" id="titulo" placeholder="Titulo Propiedad">
+
+                <label for="precio">Precio:</label>
+                <input type="number" id="precio" placeholder="Precio Propiedad">
+
+                <label for="imagen">Imagen:</label>
+                <input type="file" id="imagen" accept="image/jpeg, image/png">
+
+                <label for="descripcion">Descripción:</label>
+                <textarea id="descripcion"></textarea>
+            </fieldset>
+
+            <fieldset>
+                <legend>Información Propiedad</legend>
+
+                <label for="habitaciones">Habitaciones:</label>
+                <input type="number" id="habitaciones" placeholder="Ej: 3" min="1" max="9">
+
+                <label for="banos">Baños:</label>
+                <input type="number" id="banos" placeholder="Ej: 3" min="1" max="9">
+
+                <label for="estacionamiento">Estacionamiento:</label>
+                <input type="number" id="estacionamiento" placeholder="Ej: 3" min="1" max="9">
+            </fieldset>
+
+            <fieldset>
+                <legend>Vendedor</legend>
+
+                <select>
+                    <option value="1">Juan</option>
+                    <option value="2">Karen</option>
+                </select>
+            </fieldset>
+            <input class="boton boton-verde" type="submit" value="Crear Propiedad">
+        </form>
     </main>
 
 <?php
