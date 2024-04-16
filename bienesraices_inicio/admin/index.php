899ce66d4eabd91f952d08a5d964e79c2b7dcd2b
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-04-16 08:56:39 -0600

    Creando los archivos para las propiedades del CRUD.

---
+++ bienesraices_inicio/admin/index.php
@@ -0,0 +1,14 @@
+<?php
+require "../includes/funciones.php";
+incluirTemplate("header");
+?>
+
+    <main class="contenedor seccion">
+        <h1>Administrador de Bienes Raíces</h1>
+
+        <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Popiedad</a>
+    </main>
+
+<?php
+incluirTemplate("footer");
+?>
--- bienesraices_inicio/includes/templates/header.php
+++ bienesraices_inicio/includes/templates/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Binenes Raíces</title>
-    <link rel="stylesheet" href="build/css/app.css">
+    <link rel="stylesheet" href="/build/css/app.css">
 </head>
 
 <body>
@@ -15,15 +15,15 @@
             <div class="barra">
                 <!-- <a href="/"> --><!-- lleva a la página principal-->
                 <a href="./">
-                    <img class="logo" src="build/img/logo.svg" alt="Logotipo de Bienes Raíces">
+                    <img class="logo" src="/build/img/logo.svg" alt="Logotipo de Bienes Raíces">
                 </a>
 
                 <div class="mobile-menu">
-                    <img src="build/img/barras.svg" alt="Icono menú responsive">
+                    <img src="/build/img/barras.svg" alt="Icono menú responsive">
                 </div>
 
                 <nav class="derecha">
-                    <img class="dark-mode-boton" src="build/img/dark-mode.svg">
+                    <img class="dark-mode-boton" src="/build/img/dark-mode.svg">
                     <nav class="navegacion">
                         <a href="nosotros.php">Nosotros</a>
                         <a href="anuncios.php">Anuncios</a>
