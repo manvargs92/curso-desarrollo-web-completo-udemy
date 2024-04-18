abe20f0d0b8a0761737250b979c80fefbb8ba06e
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-04-18 13:20:11 -0600

    Validar el formulario.

--- bienesraices_inicio/admin/propiedades/crear.php
+++ bienesraices_inicio/admin/propiedades/crear.php
@@ -8,10 +8,14 @@ $db = conectarDB();
 // var_dump($_SERVER);
 // echo "</pre>";
 
+/* Mensajes de error */
+$errores = [];
+
+/* Ejecutar el código después de que el usuario envíe el formulario */
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    echo "<pre>";
-    var_dump($_POST);
-    echo "</pre>";
+    // echo "<pre>";
+    // var_dump($_POST);
+    // echo "</pre>";
 
     $titulo = $_POST["titulo"];
     $precio = $_POST["precio"];
@@ -21,15 +25,47 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $estacionamiento = $_POST["estacionamiento"];
     $vendedorId = $_POST["vendedorId"];
 
-    /* Insertar en la BD */
-    $query = "INSERT INTO propiedades (titulo, precio, descripcion, habitaciones, banos, estacionamiento, vendedores_id) VALUES ('$titulo', '$precio', '$descripcion', '$habitaciones', '$banos', '$estacionamiento', '$vendedorId');";
-    // echo $query;
-
-    $resultado = mysqli_query($db, $query);
-
-    if ($resultado) {
-        echo "Insertado correctamente.";
+    /* Validador de campos */
+    if (!$titulo) {
+        $errores[] = "Debes añadir un título";
     }
+    if (!$precio) {
+        $errores[] = "El precio es obligatorio";
+    }
+    if (strlen($descripcion) < 50) {
+        $errores[] = "La descripcion es obligatoria, y debe tener al menos 50 caracteres";
+    }
+    if (!$habitaciones) {
+        $errores[] = "El número de habitaciones es obligatorio";
+    }
+    if (!$banos) {
+        $errores[] = "El número de baños es obligatorio";
+    }
+    if (!$estacionamiento) {
+        $errores[] = "El número de lugares de estacionamiento es obligatorio";
+    }
+    if (!$vendedorId) {
+        $errores[] = "Elige un vendedor";
+    }
+    // echo "<pre>";
+    // var_dump($errores);
+    // echo "</pre>";
+
+    /* Revisar que el arreglo de $errores esté vacío */
+    if (empty($errores)) {
+
+    /* Insertar en la BD */
+        $query = "INSERT INTO propiedades (titulo, precio, descripcion, habitaciones, banos, estacionamiento, vendedores_id) VALUES ('$titulo', '$precio', '$descripcion', '$habitaciones', '$banos', '$estacionamiento', '$vendedorId');";
+        // echo $query;
+    
+        $resultado = mysqli_query($db, $query);
+    
+        if ($resultado) {
+            echo "Insertado correctamente.";
+        }
+
+    }
+
 
 }
 
@@ -42,6 +78,16 @@ incluirTemplate("header");
         <h1>Crear</h1>
 
         <a href="/admin" class="boton boton-verde">Volver</a>
+
+        <?php
+        foreach ($errores as $error) {
+            ?>
+            <div class="alerta error">
+                <?php echo $error; ?>
+            </div>
+            <?php
+        }        
+        ?>
 
         <form class="formulario" method="POST" action="/admin/propiedades/crear.php">
             <fieldset>
@@ -77,6 +123,7 @@ incluirTemplate("header");
                 <legend>Vendedor</legend>
 
                 <select name="vendedorId">
+                    <option value="" disabled selected>-- Seleccione un vendedor --</option>
                     <option value="1">Juan</option>
                     <option value="2">Karen</option>
                 </select>
