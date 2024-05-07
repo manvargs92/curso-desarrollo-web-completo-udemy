613acec912ce58caea898cf2310b47dd75d3cc50
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-05-07 12:38:01 -0600

    Creando una tabla para listar las propiedades almacenandas en la BD.

--- bienesraices_inicio/admin/index.php
+++ bienesraices_inicio/admin/index.php
@@ -24,6 +24,30 @@ incluirTemplate("header");
         <?php endif; ?>
 
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Popiedad</a>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Título</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Casa en la playa</td>
+                    <td><img src="/imagenes/1ccc3f259e5aa17ff7af8f7ab5aae5b2.jpg" class="imagen-tabla"></td>
+                    <td>$1200000</td>
+                    <td>
+                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                        <a href="#" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </main>
 
 <?php
