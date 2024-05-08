f8fa4506088f18d0fc0a3674bdc0fde2eb19db4e
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-05-08 16:25:08 -0600

    Eliminar propiedades y sus imágenes.

--- bienesraices_inicio/admin/index.php
+++ bienesraices_inicio/admin/index.php
@@ -1,4 +1,9 @@
 <?php
+
+// echo "<pre>";
+// var_dump($_POST);
+// echo "</pre>";
+
 /* Importar la conexión a la BD */
 require "../includes/config/database.php";
 $db = conectarDB();
@@ -18,6 +23,31 @@ $resultadoConsulta = mysqli_query($db, $query);
 // $mensaje = $_GET["mensaje"];
 $resultado = $_GET["resultado"] ?? null; // ?? - si no existe el valor de la izq, le asigna el de la der
 
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $id = $_POST["id"]; // el POST no va a existir hasta que se envíe el REQUEST_METHOD
+    $id = filter_var($id, FILTER_VALIDATE_INT); // valida que el value sea un int y no se le pueda pasar una cadena ej. "delete * from propiedades"
+
+    if ($id) {
+        // Eliminar el archivo
+        $query = "SELECT imagen FROM propiedades WHERE id = ${id};";
+
+        $resultado = mysqli_query($db, $query);
+        $propiedad = mysqli_fetch_assoc($resultado);
+
+        unlink("../imagenes/" . $propiedad["imagen"]);
+
+        // Eliminar la propiedad
+        $query = "DELETE FROM propiedades WHERE id = ${id};";
+        $resultado = mysqli_query($db, $query);
+
+        if ($resultado) {
+            header("location: /admin?resultado=3");
+        }
+    }
+
+    var_dump($id);
+}
+
 /* Incluir un template */
 require "../includes/funciones.php";
 incluirTemplate("header");
@@ -36,6 +66,8 @@ incluirTemplate("header");
             <p class="alerta exito">Anuncio Creado Correctamente</p>
         <?php elseif(intval($resultado) === 2): ?>
             <p class="alerta exito">Anuncio Actualizado Correctamente</p>
+        <?php elseif(intval($resultado) === 3): ?>
+            <p class="alerta exito">Anuncio Eliminado Correctamente</p>
         <?php endif; ?>
 
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Popiedad</a>
@@ -58,7 +90,10 @@ incluirTemplate("header");
                     <td><img src="/imagenes/<?php echo $propiedad["imagen"]; ?>" class="imagen-tabla"></td>
                     <td>$<?php echo $propiedad["precio"]; ?></td>
                     <td>
-                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                        <form method="POST">
+                            <input type="hidden" name="id" value="<?php echo $propiedad["id"]; ?>"><!-- input hidden - no es visible pero tiene el id que queremos eliminar; enviar datos de forma oculta -->
+                            <input type="submit" class="boton-rojo-block" value="Eliminar">
+                        </form>
                         <a
                         href="admin/propiedades/actualizar.php?id=<?php echo $propiedad["id"]; ?>" 
                         class="boton-amarillo-block">
